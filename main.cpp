d692fe618077c330feeff6853aad827c5fb4e312
WadaKeita <k023g0069@g.neec.ac.jp>
2024-07-04 20:09:16 +0900

    vector2,vector3の演算子オーバーロードを実装

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ const char kWindowTitle[] = "LD2A_02_ワダ_ケイタ";
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	const int kWindowWidth = 1280;
-	const int kWindowHeight = 720;
+	//const int kWindowWidth = 1280;
+	//const int kWindowHeight = 720;
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
@@ -19,47 +19,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = { 0 };
 	char preKeys[256] = { 0 };
-
-	// カメラ
-	Camera camera{
-		 { 1.0f,1.0f,1.0f },
-		 { 0.0f,1.9f,-6.49f },
-		 { 0.26f,0.0f,0.0f },
-		0.05f,
-		0.01f,
-	};
-
-
-	Vector3 translates[3] = {
-		{0.2f, 1.0f, 0.0f},
-		{0.4f, 0.0f, 0.0f},
-		{0.3f, 0.0f, 0.0f},
-	};
-	Vector3 rotates[3] = {
-		{0.0f, 0.0f, -6.8f},
-		{0.0f, 0.0f, -1.4f},
-		{0.0f, 0.0f, 0.0f},
-	};
-	Vector3 scales[3] = {
-		{1.0f, 1.0f, 1.0f},
-		{1.0f, 1.0f, 1.0f},
-		{1.0f, 1.0f, 1.0f},
-	};
-
-
-	Sphere sphere[3];
-	sphere[0].radius = 0.1f;
-	sphere[1].radius = 0.1f;
-	sphere[2].radius = 0.1f;
-
-	sphere[0].center = { 0,0,0 };
-	sphere[1].center = { 0,0,0 };
-	sphere[2].center = { 0,0,0 };
-
-	unsigned int sphere_color[3];
-	sphere_color[0] = RED;
-	sphere_color[1] = GREEN;
-	sphere_color[2] = BLUE;
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -74,79 +33,66 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
+		Vector2 v2a{ 0.2f, 1.0f };
+		Vector2 v2b{ 2.4f, 3.1f };
+		Vector2 v2c = v2a + v2b;
+		Vector2 v2d = v2a - v2b;
+		Vector2 v2e = v2a * 2.4f;
+		Vector2 v2f = 2.4f * v2a;
+		Vector2 v2g = v2a / 0.1f;
+		Vector2 v2h = +v2a;
+		Vector2 v2i = -v2a;
 
-		CameraOperation(camera, keys);
-
-		Matrix4x4 localMatrix[3];
-		localMatrix[0] = MakeAffineMatrix(scales[0], rotates[0], translates[0]);
-		localMatrix[1] = MakeAffineMatrix(scales[1], rotates[1], translates[1]);
-		localMatrix[2] = MakeAffineMatrix(scales[2], rotates[2], translates[2]);
-
-		Matrix4x4 worldMatrix[3];
-		worldMatrix[0] = localMatrix[0];
-		worldMatrix[1] = Multiply(localMatrix[1], worldMatrix[0]);
-		worldMatrix[2] = Multiply(localMatrix[2], worldMatrix[1]);
+		Vector3 v3a{ 0.2f, 1.0f, 0.0f };
+		Vector3 v3b{ 2.4f, 3.1f, 1.2f };
+		Vector3 v3c = v3a + v3b;
+		Vector3 v3d = v3a - v3b;
+		Vector3 v3e = v3a * 2.4f;
+		Vector3 v3f = 2.4f * v3a;
+		Vector3 v3g = v3a / 0.1f;
+		Vector3 v3h = +v3a;
+		Vector3 v3i = -v3a;
 
 
-		Matrix4x4 cameraMatrix = MakeAffineMatrix(camera.scale, camera.rotate, camera.translate);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
-		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
-		//Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+		///
+		/// ↓更新処理ここから
+		///
 
 
-		Segment segment[2];
-		segment[0].origin = GetWorldPosition(worldMatrix[0]);
-		segment[0].diff = Subtract(GetWorldPosition(worldMatrix[1]), GetWorldPosition(worldMatrix[0]));
+		ImGui::Begin("Vector2");
+		ImGui::Text("vec2a		:%f, %f", v2a.x, v2a.y);
+		ImGui::Text("vec2b		:%f, %f", v2b.x, v2b.y);
+		ImGui::Text("add		:%f, %f", v2c.x, v2c.y);
+		ImGui::Text("subtract	:%f, %f", v2d.x, v2d.y);
+		ImGui::Text("Multiply	:%f, %f", v2e.x, v2e.y);
+		ImGui::Text("Multiply2	:%f, %f", v2f.x, v2f.y);
+		ImGui::Text("division	:%f, %f", v2g.x, v2g.y);
+		ImGui::Text("plus		:%f, %f", v2h.x, v2h.y);
+		ImGui::Text("minus		:%f, %f", v2i.x, v2i.y);
 
-		segment[1].origin = GetWorldPosition(worldMatrix[1]);
-		segment[1].diff = Subtract(GetWorldPosition(worldMatrix[2]), GetWorldPosition(worldMatrix[1]));
+		ImGui::End();
+
+		ImGui::Begin("Vector3");
+		ImGui::Text("add		:%f, %f, %f", v3c.x, v3c.y, v3c.z);
+		ImGui::Text("subtract	:%f, %f, %f", v3d.x, v3d.y, v3d.z);
+		ImGui::Text("Multiply	:%f, %f, %f", v3e.x, v3e.y, v3e.z);
+		ImGui::Text("Multiply2	:%f, %f, %f", v3f.x, v3f.y, v3f.z);
+		ImGui::Text("division	:%f, %f, %f", v3g.x, v3g.y, v3g.z);
+		ImGui::Text("plus		:%f, %f, %f", v3h.x, v3h.y, v3h.z);
+		ImGui::Text("minus		:%f, %f, %f", v3i.x, v3i.y, v3i.z);
+
+
+		ImGui::End();
+
 
 		///
 		/// ↑更新処理ここまで
 		///
-
+		
 		///
 		/// ↓描画処理ここから
 		///
 
-		// グリッド描画
-		DrawGrid(viewProjectionMatrix, viewportMatrix);
-
-		DrawSegment(segment[0], viewProjectionMatrix, viewportMatrix, WHITE);
-		DrawSegment(segment[1], viewProjectionMatrix, viewportMatrix, WHITE);
-
-		DrawSphere(sphere[0], Multiply(worldMatrix[0],viewProjectionMatrix), viewportMatrix, sphere_color[0]);
-		DrawSphere(sphere[1], Multiply(worldMatrix[1], viewProjectionMatrix), viewportMatrix, sphere_color[1]);
-		DrawSphere(sphere[2], Multiply(worldMatrix[2], viewProjectionMatrix), viewportMatrix, sphere_color[2]);
-
-
-		ImGui::Begin("camera");
-		ImGui::DragFloat3("scale", &camera.scale.x, 0.01f);
-		ImGui::DragFloat3("Translation", &camera.translate.x, 0.01f);
-		ImGui::DragFloat3("rotate", &camera.rotate.x, 0.01f);
-		ImGui::DragFloat("translateSpeed", &camera.translateSpeed, 0.01f);
-		ImGui::DragFloat("rotateSpeed", &camera.rotateSpeed, 0.01f);
-		ImGui::End();
-
-
-		ImGui::Begin("sphere");
-
-		ImGui::DragFloat3("translates[0]", &translates[0].x, 0.01f);
-		ImGui::DragFloat3("rotates[0]", &rotates[0].x, 0.01f);
-		ImGui::DragFloat3("scales[0]", &scales[0].x, 0.01f);
-
-		ImGui::DragFloat3("translates[1]", &translates[1].x, 0.01f);
-		ImGui::DragFloat3("rotates[1]", &rotates[1].x, 0.01f);
-		ImGui::DragFloat3("scales[1]", &scales[1].x, 0.01f);
-		
-		ImGui::DragFloat3("translates[2]", &translates[2].x, 0.01f);
-		ImGui::DragFloat3("rotates[2]", &rotates[2].x, 0.01f);
-		ImGui::DragFloat3("scales[2]", &scales[2].x, 0.01f);
-
-
-		ImGui::End();
 
 		///
 		/// ↑描画処理ここまで
